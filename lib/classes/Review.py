675409d7229866456c6a9145cff88acfcc215713
+ <+>
2023-03-03 08:46:29 -0800

    50% done yay

--- lib/classes/Review.py
+++ lib/classes/Review.py
@@ -7,8 +7,12 @@ class Review:
         self.rating = rating
     
     # rating property goes here!
-    # def rating(self):
-    #     return self.rating 
+    def get_rating(self):
+        return self._rating
+    def set_rating(self, rating):
+        if isinstance(rating, int) and 1 <= (rating) <= 5:
+            self._rating = rating
+    rating = property(get_rating, set_rating)
 
     # viewer property goes here!
     def get_viewer(self):
